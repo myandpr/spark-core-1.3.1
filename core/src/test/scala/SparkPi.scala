958f5cda570513741ea25fe0babaa564e1b7d84a
yanmin <yanm@chinatelecom.cn>
2020-06-04 14:49:48 +0800

    sparkListener

--- core/src/test/scala/SparkPi.scala
+++ core/src/test/scala/SparkPi.scala
@@ -16,17 +16,37 @@
  */
 
 import scala.math.random
-
 import org.apache.spark._
+import org.apache.spark.scheduler.{SparkListener, SparkListenerApplicationEnd, SparkListenerApplicationStart, SparkListenerJobStart}
 
+//import org.apache.spark.Logging
+//import org.apache.spark.scheduler.{SparkListener, SparkListenerApplicationEnd, SparkListenerApplicationStart}
+class myListener extends SparkListener with Logging {
+  override def onApplicationStart(applicationStart: SparkListenerApplicationStart): Unit = {
+    //val appId = applicationStart.appId
+    print("appId"+applicationStart.appId)
+
+  }
+
+  override def onApplicationEnd(applicationEnd: SparkListenerApplicationEnd): Unit = {
+    println("app end time " + applicationEnd.time)
+    //SparkEnv.get.actorSystem.actorOf()
+  }
+
+
+}
 /** Computes an approximation to pi */
 object SparkPi {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Spark Pi").setMaster("local[*]")
     val spark = new SparkContext(conf)
+
+
+    spark.addSparkListener(new myListener)
+
     val slices = if (args.length > 0) args(0).toInt else 2
     val n = math.min(100000L * slices, Int.MaxValue).toInt // avoid overflow
-    val count = spark.parallelize(1 until n, slices).map { i =>
+    val count = spark.parallelize(1 until n, slices).cache().map { i =>
       val x = random * 2 - 1
       val y = random * 2 - 1
       if (x*x + y*y < 1) 1 else 0
